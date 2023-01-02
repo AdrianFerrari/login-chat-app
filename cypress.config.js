2030f157d820b8992e699d21a6f5f34d9113112f
Adrian Ferrari <sdadrian@gmail.com>
2023-01-01 21:35:45 -0300

    .

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,20 @@
 import { defineConfig } from "cypress";
+import dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig({
+  env: {
+    'my-var': 'ok',
+  },
   e2e: {
     baseUrl: 'http://localhost:4000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.env = config.env || {}
+      config.env.ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET
+      config.env.MONGODB_URI = process.env.MONGODB_URI
+      config.env.REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET
+
+      return config
     },
     video: false,
   },
